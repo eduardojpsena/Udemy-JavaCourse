91cdaa889e739338f2c7abd7baed7e5d6b00266e
Eduardo Sena <eduardojpsena@gmail.com>
2021-07-19 13:35:50 -0300

    refatorando o programa de leitura de arquivo para instanciar e fechar as variaveis filereader e buffered reader com dentro do bloco try

--- 21.LeituraArquivo_FileReader&BufferedReader/src/application/Program.java
+++ 21.LeituraArquivo_FileReader&BufferedReader/src/application/Program.java
@@ -10,12 +10,7 @@ public class Program {
 
 		String path = "D:\\TesteLeitura.txt";
 		
-		FileReader fr = null;
-		BufferedReader br = null;
-		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 			
 			String line = br.readLine();
 			while(line != null) {
@@ -24,17 +19,6 @@ public class Program {
 			}
 		} catch (IOException e){
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 		
 	}
