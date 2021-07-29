490026438dc4e36f9338fe1ea15aaff126b8c3ce
Eduardo Sena <eduardojpsena@gmail.com>
2021-07-29 15:34:23 -0300

    Exemplo com delimitação de metodos Generics

---
+++ 31.DelimitedGenerics/src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import model.entities.Product;
+import model.services.CalculationService;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+Locale.setDefault(Locale.US);
+		
+		List<Product> list = new ArrayList<>();
+
+		String path = "C:\\temp\\in.txt";
+
+		
+		list.add(new Product("Computer", 890.50));
+		list.add(new Product("IPhone", 910.00));
+		list.add(new Product("Tablet", 550.00));
+				
+			
+		Product x = CalculationService.max(list);
+		System.out.println("Most expensive:");
+		System.out.println(x);
+
+		
+	}
+
+}
