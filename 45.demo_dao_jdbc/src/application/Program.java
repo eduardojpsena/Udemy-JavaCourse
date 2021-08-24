bfa51a4cf0766ec7ff9ce2f92373e11fb398319a
Eduardo Sena <eduardojpsena@gmail.com>
2021-08-24 17:02:43 -0300

    implementação findbyid

--- 45.demo_dao_jdbc/src/application/Program.java
+++ 45.demo_dao_jdbc/src/application/Program.java
@@ -3,14 +3,20 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Connection connection = DB.getConnection();
+		SellerDao dao = DaoFactory.createSellerDao();
 		
+		Seller seller = dao.findById(3);
 		
+		System.out.println(seller);
 		
 	}
 
