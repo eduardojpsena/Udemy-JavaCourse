a2e0a474207592a24be2ec101689c1ba4e368ddb
Eduardo Sena <eduardojpsena@gmail.com>
2021-08-25 13:41:58 -0300

    implementação seller update

--- 45.demo_dao_jdbc/src/application/Program.java
+++ 45.demo_dao_jdbc/src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -15,28 +14,34 @@ public class Program {
 		SellerDao dao = DaoFactory.createSellerDao();
 		
 		
-		System.out.println("\n== Seller teste findById ==");
+		System.out.println("\n== Seller findById ==");
 		Seller seller = dao.findById(2);
 		
 		System.out.println(seller);
 		
-		System.out.println("\n== Seller teste findByIdDepartment ==");
+		System.out.println("\n== Seller findByIdDepartment ==");
 		Department dep = new Department(3, null);
 		List<Seller> list = dao.findbyDepartment(dep);
 		for (Seller s : list) {
 			System.out.println(s);
 		}
 		
-		System.out.println("\n== Seller teste findAll ==");
+		System.out.println("\n== Seller findAll ==");
 		list = dao.findAll();
 		for (Seller s : list) {
 			System.out.println(s);
 		}
 		
-		System.out.println("\n== Seller teste insert ==");
+		/*System.out.println("\n== Seller insert ==");
 		Seller newSeller = new Seller(null, "Carol Moom", "carol@gmail.com", new Date(), 4200.00, dep);
 		dao.insert(newSeller);
-		System.out.println("Inserted - new id: " + newSeller.getId());
+		System.out.println("Inserted - new id: " + newSeller.getId());*/
+		
+		System.out.println("\n== Seller update ==");
+		seller = dao.findById(1);
+		seller.setBaseSalary(10000.00);
+		dao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
