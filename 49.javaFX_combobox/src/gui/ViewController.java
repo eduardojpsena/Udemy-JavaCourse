05eeaa8b79ac53a592db4fb3faed84e97ada732c
Eduardo Sena <eduardojpsena@gmail.com>
2021-08-29 14:18:24 -0300

    java fx comboBox

---
+++ 49.javaFX_combobox/src/gui/ViewController.java
@@ -0,0 +1,63 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import model.entities.Person;
+
+public class ViewController implements Initializable {
+
+	@FXML
+	private ComboBox<Person> comboBoxPerson;
+
+	private ObservableList<Person> obsList;
+	
+	@FXML
+	private Button btAll;
+	
+	@FXML
+	public void onComboBoxPerson() {
+		Person person = comboBoxPerson.getSelectionModel().getSelectedItem();
+		System.out.println(person);
+	}
+	
+	@FXML
+	public void onBtAllAction() {
+		for (Person person : comboBoxPerson.getItems()) {
+			System.out.println(person);
+		}
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+		List<Person> list = new ArrayList<Person>();
+		list.add(new Person(1, "Eduardo", "eduardo@email.com"));
+		list.add(new Person(2, "Andre", "andre@email.com"));
+		list.add(new Person(3, "Alex", "alex@email.com"));
+
+		obsList = FXCollections.observableArrayList(list);
+		comboBoxPerson.setItems(obsList);
+
+		Callback<ListView<Person>, ListCell<Person>> factory = lv -> new ListCell<Person>() {
+			@Override
+			protected void updateItem(Person item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getName());
+			}
+		};
+		comboBoxPerson.setCellFactory(factory);
+		comboBoxPerson.setButtonCell(factory.call(null));
+	}
+}
